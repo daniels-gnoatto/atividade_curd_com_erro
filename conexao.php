75b2118d4c3ca9945393c60a4535ef4d4b46f25e
Daniels-gnoatto <daniel_gnoatto@estudante.sesisenai.org.br>
2025-08-11 22:13:50 -0300

    feito

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php
+// Conexão com o banco (contém erro de variável e de conexão)
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "crud_exemplo";
+
+$conn = new mysql($host, $user, $password, $db);
+
+if ($conn->connect_error) {
+    die "Falha na conexão!";
+}
+?>
